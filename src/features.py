63da3f4a268c7598e81c0c2a7110540de3471b19
Ridhima Sinare <ridhisinare@gmail.com>
2024-03-12 23:27:30 +0530

    Added features.py

---
+++ src/features.py
@@ -0,0 +1,71 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import argparse
+import pandas as pd
+import yaml
+
+def extract(URL):
+    client_id = "5cd36d4447db4cc48f1fce11d381d795" 
+    client_secret = "1e3d380b95764336bde5485bf0512a4f"
+
+    #use the clint secret and id details
+    client_credentials_manager = SpotifyClientCredentials(client_id=client_id,client_secret=client_secret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    # the URI is split by ':' to get the username and playlist ID
+    playlist_id = URL.split("/")[4].split("?")[0]
+    playlist_tracks_data = sp.playlist_tracks(playlist_id)
+
+    #lists that will be filled in with features
+    playlist_tracks_id = []
+    playlist_tracks_titles = []
+    playlist_tracks_artists = []
+    playlist_tracks_first_artists = []
+
+    #go through the dictionary to extract the data
+    for track in playlist_tracks_data['items']:
+        playlist_tracks_id.append(track['track']['id'])
+        playlist_tracks_titles.append(track['track']['name'])
+        # adds a list of all artists involved in the song to the list of artists for the playlist
+        artist_list = []
+        for artist in track['track']['artists']:
+            artist_list.append(artist['name'])
+        playlist_tracks_artists.append(artist_list)
+        playlist_tracks_first_artists.append(artist_list[0])
+
+    #create a dataframe
+    features = sp.audio_features(playlist_tracks_id)
+    features_df = pd.DataFrame(data=features, columns=features[0].keys())
+    features_df['title'] = playlist_tracks_titles
+    features_df['first_artist'] = playlist_tracks_first_artists
+    features_df['all_artists'] = playlist_tracks_artists
+    features_df = features_df[['id', 'title', 'first_artist', 'all_artists',
+                                'danceability', 'energy', 'key', 'loudness',
+                                'mode', 'acousticness', 'instrumentalness',
+                                'liveness', 'valence', 'tempo',
+                                'duration_ms', 'time_signature']]
+    
+    return features_df
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+
+def get_features(config_path):
+    config = read_params(config_path)
+    # print(config)
+    url = config["playlist_url"]
+    feature_df = extract(url)
+    return feature_df
+
+
+
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config",default="params.yaml")
+    parsed_args = args.parse_args()
+    features = get_features(config_path = parsed_args.config)
+    print(features)
