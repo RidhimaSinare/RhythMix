815041e3f77788d36db35433d0d3d4f8fab5c1e4
Ridhima Sinare <ridhisinare@gmail.com>
2024-04-05 23:58:48 +0530

    Added MusicReco Funcionality

--- app.py
+++ app.py
@@ -60,15 +60,20 @@ def login():
 
     return render_template('login.html')
 
-@app.route('/home')
+@app.route('/home',methods=['GET','POST'])
 def home():
+
+    if request.method=='POST':
+        url = request.form['url']
+        top5 = recommend(url)
+        return render_template("home.html", top5=top5)
+
     
     return render_template("home.html")
 
-# @app.route('/recommend', methods=['POST'])
-def recommend():
+def recommend(URL):
    #requesting the URL form the HTML form
-   URL = "https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M"
+#    URL = "https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M"
    #using the extract function to get a features dataframe
    df = extract(URL)
    #retrieve the results and get as many recommendations as the user requested
@@ -77,9 +82,11 @@ def recommend():
    my_songs = []
    for i in range(number_of_recs):
       my_songs.append([str(edm_top40.iloc[i,1]) + ' - '+ '"'+str(edm_top40.iloc[i,4])+'"', "https://open.spotify.com/track/"+ str(edm_top40.iloc[i,-6]).split("/")[-1]])
-   print(my_songs)
+#    print(my_songs)
+   return my_songs
+
 
 if __name__=="__main__":
    
-   # recommend()
+#    recommend()
    app.run(debug=True, host='127.0.0.1',port=5000)
